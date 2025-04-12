c7622528186bd914175395a329beb8bf5ad3ec82
PallaviZadokar <143967127+PallaviZadokar@users.noreply.github.com>
2025-04-12 12:50:23 +0530

    Update classify.py

--- backend/classify.py
+++ backend/classify.py
@@ -6,12 +6,12 @@ from langchain.schema import HumanMessage
 import os
 
 llm = ChatGroq(
-    model="Gemma2-9b-It",
+    model="llama-3.1-8b-instant",
     temperature=0,
     api_key=os.getenv("GROQ_API_KEY")
 )
 
 def classify_document_type(text: str):
-    doc_type_prompt = f"What type of document is this? Choose one: Bill of Exchange, GRN, PO.\n\nText:\n{text}"
+    doc_type_prompt = f"What type of document is this? Choose one: Bill of Exchange, Goods Receipt Note, Purchase Order.\n\nText:\n{text}"
     response = llm([HumanMessage(content=doc_type_prompt)])
     return response.content.strip()
